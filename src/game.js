55a789ac2caafae10c49e8018f22bff9672471c1
SHogenboom <28568743+SHogenboom@users.noreply.github.com>
2023-12-26 12:09:43 +0100

    ADD BASIC STYLING
    Setup to be able to add more styling to the game. Now just a
    basic colored background and centering of game-div.

--- src/game.js
+++ src/game.js
@@ -1,13 +1,15 @@
-// import kaboom lib
-import kaboom from "kaboom";
+// IMPORTS
+import kaboom from "kaboom"; // javascript library for making games
+import "./style.css"; // custom styling
 
 // GLOBAL VARIABLES// 540 x 960 pixels, 16:9 ratio
 const gameWidth = 960;
 const gameHeight = 540;
 
 // CREATE GAME
-kaboom({
+const k = kaboom({
   width: gameWidth,
   height: gameHeight,
   canvas: document.querySelector("#game"),
+  global: false,
 });
